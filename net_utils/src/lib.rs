5c7a11d7ee4a779cc1bc86db2e329b4d58c1ca8a
tytjan <adrianczech-1997@wp.pl>
2022-06-11 00:23:28 +0200

    Second commit

--- net_utils/src/lib.rs
+++ net_utils/src/lib.rs
@@ -1,1 +1,22 @@
 pub mod tcp_port;
+
+extern crate dns_lookup;
+use request_config::request_config::RequestConfig;
+use tcp_port::TcpPort;
+use dns_lookup::lookup_host;
+
+pub fn get_request_address(rc: &RequestConfig) -> String
+{
+    let port = TcpPort::from_protocol(&rc.get_protocol().unwrap());
+    let website = rc.get_website();
+
+    let ip_vec = lookup_host(website).unwrap();
+    let mut addr = String::new();
+    for i in ip_vec
+    {
+        addr = addr + &i.to_string();
+    }
+
+    addr = addr + ":" + &port.as_string();
+    return addr;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use request_config::request_config::*;
-use net_utils::tcp_port;
-use tcp_port::TcpPort;
-use dns_lookup::lookup_host;
+use net_utils::get_request_address;
 
-fn get_request_address(rc: &RequestConfig) -> String
-{
-    let port = TcpPort::from_protocol(&rc.get_protocol().unwrap());
-    let website = rc.get_website();
-
-    let ip_vec = lookup_host(website).unwrap();
-    let mut addr = String::new();
-    for i in ip_vec
-    {
-        addr = addr + &i.to_string();
-    }
-
-    addr = addr + ":" + &port.as_string();
-    return addr;
-}
 
 #[allow(unused_variables)]
 fn main()
 {
-    let url = "http://www.forum-jagiellonii.pl/viewtopic.php?t=2373&sid=a0bff4cfedc7a059ab4424cb54ab5315&start=780".to_string();
+    let url = "http://www.forum-jagiellonii.pl/\
+    viewtopic.php?t=2373&sid=a0bff4cfedc7a059ab4424cb54ab5315&start=780".to_string();
+
     let rq = RequestConfig::new(url);
 
     let ra = get_request_address(&rq);
